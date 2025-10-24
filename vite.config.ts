4e17d7665b0501debe1668e9fd33433c1fb220b5
gerardofigueroadev <gerardo.figueroa.dev@gmail.com>
2025-10-23 22:42:39 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   // Use the repository name as base path in production so assets resolve on GitHub Pages.
-  base: process.env.NODE_ENV === 'production' ? '/sellyx-front/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/sellyx-front/' : '/', 
   plugins: [vue()],
 })
